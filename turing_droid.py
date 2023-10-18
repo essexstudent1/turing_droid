58dc120d5daa4921d41b07c8071e90bc35b54f3d
Kate W <94301901+essexstudent1@users.noreply.github.com>
2023-10-18 13:01:07 -0400

    Update turing_droid.py

--- turing_droid.py
+++ turing_droid.py
@@ -18,6 +18,7 @@ import random
 
 from threading import Thread
 from queue import Queue
+from cleantext import clean
 
 # Import Machne learning libraries and modules
 import numpy as np
@@ -401,17 +402,7 @@ def remove_emoji(text):
         Returns:
             The cleaned text.
     """
-    emoji_pattern = re.compile(
-        '['
-        u'\U0001F600-\U0001F64F'  # emoticons
-        u'\U0001F300-\U0001F5FF'  # symbols & pictographs
-        u'\U0001F680-\U0001F6FF'  # transport & map symbols
-        u'\U0001F1E0-\U0001F1FF'  # flags (iOS)
-        u'\U00002702-\U000027B0'
-        u'\U000024C2-\U0001F251'
-        ']+',
-        flags=re.UNICODE)
-    return emoji_pattern.sub(r'', text)
+    return clean(text, no_emoji=True))
 
 def stemming(text):
     """ Function to stem words in text.
